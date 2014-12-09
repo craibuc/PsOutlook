0f405ff67ebaa349658f71e76718c749409f41e7
unknown <cb2215@CHOAW7-IST006.choa.org>
2014-12-09 08:25:26 -0500

    Initial commit

--- PsOutlook.psm1
+++ PsOutlook.psm1
@@ -1,9 +1,41 @@
 # Add-Type -assembly "Microsoft.Office.Interop.Outlook"
 
+<#
+.SYNOPSIS
+    Creates and sends an Outlook mail message.
+
+.DESCRIPTION
+    Creates an Outlook mail message, then sends it.  Optionally, preview the message instead of sending it.
+
+.PARAMETER To
+    The recipient(s).  Separate mutliple recipients with a semi-colon (;).
+
+.PARAMETER CC
+    The "courtesy copy" recipient(s).  Separate mutliple recipients with a semi-colon (;).
+
+.PARAMETER BCC
+    The "blind courtesy copy" recipient(s).  Separate mutliple recipients with a semi-colon (;).
+
+.PARAMETER Subject
+    The message's subject.
+
+.PARAMETER Body
+    The message's body (supports embedded HTML).
+
+.PARAMETER Attachments
+    Comma-delimited list of attachments.
+
+.PARAMETER Preview
+    If set, the message will be displayed instead of sent.
+
+.EXAMPLE
+    Send-Mail "recipient@domain0.tld;recipient@domain1.tld", "the subject", "the message", "path/to/attachment0;path/to/attachmentN"
+    
+#>
+
 Function Send-Mail {
 
     [cmdletbinding()]
-    
     param (
         [Parameter(Mandatory=$True,Position=0)][String] $To,
         [Parameter(Mandatory=$True,Position=1)][String] $Subject,
@@ -16,22 +48,17 @@ Function Send-Mail {
     
     begin {
         Write-Verbose "$($MyInvocation.MyCommand.Name)::Begin"
-    }
-    
-    process {
-
-        $outlook = $null
 
         try {
             # activate existing instance
-            $outlook = [Runtime.InteropServices.Marshal]::GetActiveObject("Outlook.Application")
-            $outlook.ActiveWindow().Activate()
+            $Outlook = [Runtime.InteropServices.Marshal]::GetActiveObject("Outlook.Application")
+            $Outlook.ActiveWindow().Activate()
         }
         catch [System.Management.Automation.MethodInvocationException] {
             # open new instance
-            #$outlook = new-object -com Outlook.Application
-            $outlook = New-Object Outlook.Application
-            $namespace = $outlook.GetNamespace("MAPI")
+            $Outlook = new-object -com Outlook.Application
+            # $Outlook = New-Object Outlook.Application
+            $namespace = $Outlook.GetNamespace("MAPI")
             $folder = $namespace.GetDefaultFolder("olFolderInbox")
             $explorer = $folder.GetExplorer()
             $explorer.Display() 
@@ -39,6 +66,28 @@ Function Send-Mail {
         catch [Exception] {
             Write-Host $_.Exception.ToString()
         }
+
+    }
+    
+    process {
+
+        # try {
+        #     # activate existing instance
+        #     $Outlook = [Runtime.InteropServices.Marshal]::GetActiveObject("Outlook.Application")
+        #     $Outlook.ActiveWindow().Activate()
+        # }
+        # catch [System.Management.Automation.MethodInvocationException] {
+        #     # open new instance
+        #     #$Outlook = new-object -com Outlook.Application
+        #     $Outlook = New-Object Outlook.Application
+        #     $namespace = $Outlook.GetNamespace("MAPI")
+        #     $folder = $namespace.GetDefaultFolder("olFolderInbox")
+        #     $explorer = $folder.GetExplorer()
+        #     $explorer.Display() 
+        # }
+        # catch [Exception] {
+        #     Write-Host $_.Exception.ToString()
+        # }
         
         # OlItemType.olMailItem = 0
         $Mail = $Outlook.CreateItem(0)
@@ -64,11 +113,11 @@ Function Send-Mail {
         #$Mail.Attachments.Add($File) | out-null
 
         if ($preview) {
-            Write-Debug "Previewing..."
+            Write-Debug "Verbose..."
             $Mail.Display()
         }
         else {
-            Write-Debug "Sending..."
+            Write-Debug "Verbose..."
             $Mail.Send()
         }
         
@@ -77,39 +126,6 @@ Function Send-Mail {
     end {
         Write-Verbose "$($MyInvocation.MyCommand.Name)::End"
     }
-    
-    <#
-    .SYNOPSIS
-        Creates and sends an Outlook mail message.
-    
-    .DESCRIPTION
-        Creates an Outlook mail message, then sends it.  Optionally, preview the message instead of sending it.
-    
-    .PARAMETER To
-        The recipient(s).  Separate mutliple recipients with a semi-colon (;).
-
-    .PARAMETER CC
-        The "courtesy copy" recipient(s).  Separate mutliple recipients with a semi-colon (;).
-    
-    .PARAMETER BCC
-        The "blind courtesy copy" recipient(s).  Separate mutliple recipients with a semi-colon (;).
-
-    .PARAMETER Subject
-        The message's subject.
-
-    .PARAMETER Body
-        The message's body (supports embedded HTML).
-
-    .PARAMETER Attachments
-        Comma-delimited list of attachments.
-
-    .PARAMETER Preview
-        If set, the message will be displayed instead of sent.
-    
-    .EXAMPLE
-        Send-Mail "recipient@domain0.tld;recipient@domain1.tld", "the subject", "the message", "path/to/attachment0;path/to/attachmentN"
-        
-    #>
     
 }
 
